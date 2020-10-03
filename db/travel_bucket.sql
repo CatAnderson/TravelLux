1e2328f2c7ad2df70d5eacb6afd3c0f207d81bf6
CatAnderson <thecatanderson100@gmail.com>
2020-10-03 13:36:38 +0100

    fixed error in travel_bucket.sql, tables can now be created

--- db/travel_bucket.sql
+++ db/travel_bucket.sql
@@ -5,12 +5,12 @@ CREATE TABLE countries (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     continent VARCHAR(255)
-)
+);
 
 CREATE TABLE cities (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    country_id INT REFERENCES countries(id)
-    visited BOOLEAN,
+    country_id INT REFERENCES countries(id),
+    visited BOOLEAN
 )
 
