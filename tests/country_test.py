1228cac87bc2c382a6383941d80c989e956a20fb
CatAnderson <thecatanderson100@gmail.com>
2020-10-03 12:57:37 +0100

    two tests run and passed in country_test.py

--- tests/country_test.py
+++ tests/country_test.py
@@ -7,5 +7,10 @@ class TestCountry(unittest.TestCase):
         self.country_1 = Country("Japan", "Asia")
         self.country_2 = Country("Germany", "Europe")
 
+
     def test_country_has_name(self):
         self.assertEqual("Japan", self.country_1.name)
+
+
+    def test_country_has_continent(self):
+        self.assertEqual("Europe", self.country_2.continent)
