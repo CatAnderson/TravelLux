c73f3bb79ed6a7d01bc0367aab8cbd85c7435057
CatAnderson <thecatanderson100@gmail.com>
2020-10-03 13:19:38 +0100

    want to test if city visited can be changed to true, test written

--- tests/city_test.py
+++ tests/city_test.py
@@ -13,10 +13,16 @@ class TestCity(unittest.TestCase):
         self.assertEqual("Tokyo", self.city_1.name)
         self.assertEqual("Berlin", self.city_3.name)
 
+
     def test_city_has_counrty(self):
         self.assertEqual("Japan", self.city_2.country)
+
 
     def test_city_visited_starts_false(self):
         self.assertEqual(False, self.city_1.visited)
 
+    
+    def test_can_change_city_visit_to_true(self):
+        self.city.mark_as_visited()
+        self.assertEqual(True, self.city_1.visited)
     
