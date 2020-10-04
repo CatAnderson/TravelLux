74cb90706839faf25b98a49958190a4e4aec81fa
CatAnderson <thecatanderson100@gmail.com>
2020-10-04 15:13:57 +0100

    delete_all method added to the console.py

--- console.py
+++ console.py
@@ -6,6 +6,8 @@ from models.country import Country
 import repositories.city_repository as city_repository
 import repositories.country_repository as country_repository
 
+
+city_repository.delete_all()
 country_repository.delete_all()
 
 
