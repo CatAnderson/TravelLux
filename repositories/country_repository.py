e3fc1b3edb7f4f75cc7c5c2cae84a6a082fafdae
CatAnderson <thecatanderson100@gmail.com>
2020-10-04 14:50:42 +0100

    delete(id) function created in country_repository.py

--- repositories/country_repository.py
+++ repositories/country_repository.py
@@ -45,3 +45,10 @@ def select(id):
 def delete_all():
     sql = "DELETE FROM countries"
     run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM countries WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+
