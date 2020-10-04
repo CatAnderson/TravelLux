befcecda5105467ea9e25e689a9355f572cc1ff7
CatAnderson <thecatanderson100@gmail.com>
2020-10-04 16:41:22 +0100

    countries_blueprint.route(/countries), setup

--- controllers/cities_controller.py
+++ controllers/cities_controller.py
@@ -4,5 +4,5 @@ from flask import Blueprint
 from models.city import City
 from models.country import Country
 
-# import repositories.city_repository as city_repository
-# import repositories.country_repository as country_repository
+import repositories.city_repository as city_repository
+import repositories.country_repository as country_repository
--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -4,5 +4,14 @@ from flask import Blueprint
 from models.country import Country
 from models.city import City
 
-# import repositories.country_repository as country_repository
-# # import repositories.city_repository as city_repository
+import repositories.country_repository as country_repository
+import repositories.city_repository as city_repository
+
+countries_blueprint = Blueprint("countries", __name__)
+
+
+@countries_blueprint.route("/countries")
+def tasks():
+    countries = country_repository.select_all()
+    return render_template("country/index.html", all_countries = countries)
+
