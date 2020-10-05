288bad8bb289a55da52b102aee6c80925df69e85
CatAnderson <thecatanderson100@gmail.com>
2020-10-05 10:12:42 +0100

    adding routes and html

--- controllers/cities_controller.py
+++ controllers/cities_controller.py
@@ -6,3 +6,10 @@ from models.country import Country
 
 import repositories.city_repository as city_repository
 import repositories.country_repository as country_repository
+
+cities_blueprint = Blueprint("cities", __name__)
+
+@cities_blueprint.route("/bucketlist")
+def tasks():
+    cities = city_repository.select_all()
+    return render_template("bucket_list/index.html", all_cities = cities)
--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -15,3 +15,8 @@ def tasks():
     countries = country_repository.select_all()
     return render_template("country/index.html", all_countries = countries)
 
+
+@countries_blueprint.route("/countries/<id>", methods=['GET'])
+def show_country(id):
+    country = country_repository.select(id)
+    return render_template('city/index.html', country = country)
