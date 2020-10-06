923dcad96f21338366bc5580cfdbaccb1dfd7677
CatAnderson <thecatanderson100@gmail.com>
2020-10-06 09:33:37 +0100

    select all function added to bucketlist_repository.py

--- repositories/bucklist_repository.py
+++ repositories/bucklist_repository.py
@@ -4,6 +4,8 @@ from models.destination import Destination
 from models.country import Country
 from models.bucketlist import Bucketlist
 
+import repositories.destination_repository as destination_repository
+
 
 def save(bucketlist):
     sql = "INSERT INTO bucketlists (destination_id) VALUES (%s) RETURNING *"
@@ -12,5 +14,18 @@ def save(bucketlist):
     id = results[0]['id']
     bucketlist.id = id
     return bucketlist
+
+def select_all():
+    bucketlists = []
+
+    sql = "SELECT * FROM bucketlists"
+    results = run_sql(sql)
+
+    for row in results:
+        destination_id = destination_repository.id
+        bucketlist_id = row["id"]
+        bucketlist = Bucketlist(destination_id, bucketlist_id)
+        bucketlists.append(bucketlist)
+    return bucketlists
 
 
